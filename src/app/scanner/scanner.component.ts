05b54cdb650709a3224c6ef36a4ebce7137950de
appelzd <appel.david@live.com>
2020-05-08 22:14:07 -0600

    adding image tag

--- src/app/scanner/scanner.component.ts
+++ src/app/scanner/scanner.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-scanner',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scanner.component.sass']
 })
 export class ScannerComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  @Output() filechanged = new EventEmitter<any>();
+
+  fileChanged(imageUploader){
+    let g = imageUploader;
+    
+    let reader = new FileReader();
+
+    reader.onload = event =>
+    {
+        this.filechanged.emit(event.target["result"]);
+    }
+
+    reader.readAsDataURL(imageUploader["files"][0]);
+  }
 }
